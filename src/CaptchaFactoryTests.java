7b660aa84fb037db1c765c95b0730fb87d18c49e
Wasith Theerapattrathamrong <wasith.t@gmail.com>
2023-12-03 14:51:37 +0700

    test: CaptchaFactory

--- src/CaptchaFactoryTests.java
+++ src/CaptchaFactoryTests.java
@@ -1,7 +1,10 @@
 import org.junit.jupiter.api.Test;
 
-import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CaptchaFactoryTests {
@@ -13,14 +16,21 @@ public class CaptchaFactoryTests {
         sut.createCaptcha();
 
         assertEquals(4, spy.nextIntWithOriginAndBoundCount);
+
+        assertEquals(new ArrayList<>(Arrays.asList(1, 1, 1, 1)), spy.nextIntOriginParams);
+        assertEquals(new ArrayList<>(Arrays.asList(3, 10, 4, 10)), spy.nextIntBoundParams);
     }
 
-    private class SecureRandomSpy implements IntegerRandomizer{
+    private static class SecureRandomSpy implements IntegerRandomizer{
         public int nextIntWithOriginAndBoundCount;
+        public ArrayList<Integer> nextIntOriginParams = new ArrayList<>();
+        public ArrayList<Integer> nextIntBoundParams = new ArrayList<>();
 
         @Override
         public int nextInt(int origin , int bound) {
             nextIntWithOriginAndBoundCount += 1;
+            nextIntOriginParams.add(origin);
+            nextIntBoundParams.add(bound);
             return -1;
         }
     }
